4de3d115db9f35d3bdfc30249195578b2ea76e53
Mohsen Banan <form@mohsen.banan.1.byname.net>
2026-07-29 18:32:56 -0700

    Install is through pypiProc.sh.

--- py3/install.ps1
+++
@@ -1,7 +0,0 @@
-#
-# This is for user files based (git) installation. For development, use pypiProc.ps1
-#
-$VerbosePreference = "Continue"
-
-pip uninstall -y polytson.soncli
-pip install . --upgrade
--- py3/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# This is for user files based (git) installation. For development, use pypiProc.sh
-#
-set -x
-#
-pip uninstall -y polytson.soncli
-pip install . --upgrade
